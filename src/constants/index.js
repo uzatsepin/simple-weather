5ec7e3f9bc6821faf9bec99af586af331658af51
Andrii Zatsepin <uzatsepin@gmail.com>
2023-07-14 17:39:30 +0300

    initial comment

---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = 'eda03305436f56376a611f3e368a72a4';
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
---
+++ src/utils/index.js
@@ -0,0 +1,6 @@
+export const capitalazeFirstLetter = (string) => {
+    if(!string) {
+        return ''
+    }
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
